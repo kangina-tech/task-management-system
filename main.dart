40c2c17538152c504bd7df0f21d4053ccf8e481f
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-05 15:27:18 +0300

    added task list class with some functionalities

--- main.dart
+++ main.dart
@@ -1,9 +1,36 @@
+ // task class
 class Task {
-  // task class
   String title;
   String description;
   DateTime dueDate;
   String status;
 
   Task({required this.title, required this.description, required this.dueDate, required this.status });
+}
+
+// task list class
+class TaskList {
+  List<Task> tasks = [];
+  // add tasks
+  void addTask(Task task) {
+    tasks.add(task);
+  }
+  // remove tasks
+  void removeTask(int index) {
+    if (index >= 0 && index < tasks.length) {
+      tasks.removeAt(index);
+    }
+  }
+  // update tasks
+  void updateTask(int index, Task updatedTask) {
+    if (index >= 0 && index < tasks.length) {
+      tasks[index] = updatedTask;
+    }
+  }
+  // display
+  void printTasks() {
+    for(int i = 0; i < tasks.length; i++) {
+      print('${i + 1}. ${tasks[i].title}');
+    }
+  }
 }
