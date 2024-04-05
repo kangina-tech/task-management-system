e0ec80d24e2840908a721ac999a573978e6f209f
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-05 14:42:45 +0300

    created task class

---
+++ main.dart
@@ -0,0 +1,9 @@
+class Task {
+  // task class
+  String title;
+  String description;
+  DateTime dueDate;
+  String status;
+
+  Task({required this.title, required this.description, required this.dueDate, required this.status });
+}
